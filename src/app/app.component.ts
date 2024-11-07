42a69c2b170bb3655e089b5d98c96ab83050a7dd
zubair farooq <zubairrehman698@gmail.com>
2024-11-07 22:08:30 +0500

    Task-1_Zubair farooq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CounterService } from './countertask/counter.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GTP';
   count: number = 1;
+
+  constructor(private counterService: CounterService) {}
+
+  ngOnInit() {
+    this.counterService.currentCount.subscribe((count) => {
+      this.count = count;
+    });
+  }
 }
