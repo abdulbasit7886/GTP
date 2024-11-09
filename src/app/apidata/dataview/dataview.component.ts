01dd0819608a5a6fa0fb69c82f0fd981216effaa
zubair672 <zubair.36aug24webgpt@gmail.com>
2024-11-09 12:51:54 +0500

    task 2 update branch done

--- src/app/apidata/dataview/dataview.component.ts
+++ src/app/apidata/dataview/dataview.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./dataview.component.css']
 })
 export class DataviewComponent {
-
+  users: any[]=[];
+  constructor(){}
+  ngOnInit() {
+    this.fetchData()
+  }
+fetchData(){
+  fetch('https://jsonplaceholder.typicode.com/users',{
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then((res)=>  res.json())
+  .then((data)=>{
+    return this.users=data
+  })
+  .catch(err=>{
+    console.error(err);
+    return err.message;
+  })
+}
 }
