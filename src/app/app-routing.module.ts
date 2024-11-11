017b2a149f290c4dbfc369766458bd1a0fb192a5
zubair farooq <zubairrehman698@gmail.com>
2024-11-11 18:40:37 +0500

    Task-6-ZubairFarooq(Calculator App)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-import { CounterComponent } from './counter/counter.component'
+import { FormsModule } from '@angular/forms';
+import { CalculatorComponent } from './calculator/calculator.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
