fb347a63b480c0b298c11d1c61fbb02ff78b5102
Fiza Khalid <fiza.khalid731@gmail.com>
2024-11-21 02:45:42 +0500

    feat: show notification when new post create

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -35,51 +35,5 @@ clearNotification() {
 
 }
 
-  // constructor() {
-  //   this.socket = io(this.serverUrl,{
-  //     transports:['websocket'],
-  //     query: { userId: '6739bc1321ef694be325d9b4' }
-  //   });
-
-  //   this.socket.on('connect',()=>{
-  //    console.log('Socket connected with ID:', this.socket.id)
-  //   });
-
-  //   this.socket.on('disconnect',()=>{
-  //     console.log('Socket disconnected');
-  //   });
-
-  //   // Handle connection errors
-  //   this.socket.on('connect_error', (error: any) =>{
-  //     console.error('Socket connection error:', error);
-  //   });
-  //  }
-
-   
-
-  //  listenNotification(): Observable<string>{
-  //   return new Observable<string>((observer) =>{
-  //     this.socket.on('notification',(message: string) =>{
-  //       observer.next(message);
-  //     });
-  //   });
-  //  }
-
-  //  // Register user when connected
-  // registerUser(userId: string) {
-  //   this.socket.emit('register', userId); // Emit the userId to register with socket.io
-  // }
-
-  // // Disconnect the socket when no longer needed
-  // disconnect() {
-  //   this.socket.disconnect();
-  //   console.log('Socket disconnected');
-  // }
-
-  // handleSocketError() {
-  //   this.socket.on('error', (error: any) => {
-  //     console.error('Socket error:', error);
-  //   });
-  // }
-
+ 
 
